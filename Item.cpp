4a44bbba4c0404d8239eb5c9e8294eb6fe336808
it21097324 <77889625+Yujitha@users.noreply.github.com>
2021-08-23 10:02:24 +0000

    complete

--- Item.cpp
+++ Item.cpp
@@ -6,6 +6,18 @@ using namespace std;
 // 5. Implement Overloaded Constructor Implementation
 // 6. Implement Destructor (display "Destructor Called")
 
+Item::Item(){
+  itemCode =0;
+  uniPrice = 0.0;
+  discount = 0;
+}
+
+Item::Item(int pitemcode , float puniPrice){
+  itemCode = pitemcode;
+  uniPrice = punitPrice;
+}
+
+
 void Item::setDiscount(float pdiscount) {
   discount = pdiscount;
 }
